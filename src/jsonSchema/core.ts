1f992a4976b900ff3550aa7e2f560d98eb8b6f77
keisuke machida <key.calling24@gmail.com>
2023-07-08 06:21:12 +0900

    chore

--- src/jsonSchema/core.ts
+++ src/jsonSchema/core.ts
@@ -12,8 +12,6 @@ export async function jsonSchemaToType(
 ) {
   const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
 
-  // We could add multiple schemas for multiple types,
-  // but here we're just making one type from JSON schema.
   await schemaInput.addSource({ name: typeName, schema: jsonSchemaString });
 
   const inputData = new InputData();
