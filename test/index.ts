ab1ec96bdd3efdf480b5b0bb517aee45acdfe4dc
keisuke machida <key.calling24@gmail.com>
2023-07-08 14:37:21 +0900

    fix: filename

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,10 @@
 import type { Schema } from "../out/type";
 
 const schema: Schema = {
-  device: {
-    colorDepth: 10,
+  web: {
+    webPageDetails: {
+      name: "hoge:foo:bar",
+    },
+    we,
   },
 };
